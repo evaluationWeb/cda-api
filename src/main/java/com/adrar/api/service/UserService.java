b338026e85c7988921b4cd1c2aa786f2d707acba
mithridatem <mathieumithridate@adrar-formation.com>
2025-07-07 16:02:16 +0200

    feat: userservice

--- src/main/java/com/adrar/api/service/UserService.java
+++ src/main/java/com/adrar/api/service/UserService.java
@@ -5,6 +5,8 @@ import com.adrar.api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
@@ -24,6 +26,12 @@ public class UserService {
     //récupérer par id
 
     //récupérer tous
+    public Iterable<User> getAllUser(){
+        if(userRepository.count() ==0) {
+            throw new RuntimeException("la liste est vide");
+        }
+        return userRepository.findAll();
+    }
 
     //récupérer par email
     public boolean getUserByEmail(String email) {
