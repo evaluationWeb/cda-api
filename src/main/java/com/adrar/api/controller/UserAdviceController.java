b61c8e0ec2e73fce1b5f160364955d82b69fa62c
mithridatem <mathieumithridate@adrar-formation.com>
2025-07-08 14:27:39 +0200

    feat: UserAdviceController : User exist, not exist

--- src/main/java/com/adrar/api/controller/UserAdviceController.java
+++ src/main/java/com/adrar/api/controller/UserAdviceController.java
@@ -1,4 +1,35 @@
 package com.adrar.api.controller;
 
+import com.adrar.api.exception.UserExistException;
+import com.adrar.api.exception.UserIsNotExistException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
 public class UserAdviceController {
+
+    @ExceptionHandler(UserExistException.class)
+    public ResponseEntity<Map<String, String>> handleUserExistException(UserExistException ex) {
+        Map response = new HashMap();
+        response.put("Error", ex.getMessage());
+        response.put("Status Code ", String.valueOf(HttpStatus.CONFLICT.value()));
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(response);
+    }
+
+    @ExceptionHandler(UserIsNotExistException.class)
+    public ResponseEntity<Map<String, String>> handleUserIsNotExistException(UserIsNotExistException ex) {
+    Map response = new HashMap();
+    response.put("Error", ex.getMessage());
+    response.put("Status Code ", String.valueOf(HttpStatus.NOT_FOUND.value()));
+    return  ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(response);
+    }
 }
