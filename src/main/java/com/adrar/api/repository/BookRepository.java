4fd0a4136aadde67360bec33c892f32fd981e766
mithridatem <mathieumithridate@adrar-formation.com>
2025-07-09 11:40:46 +0200

    feat : repository book, category, author, user

---
+++ src/main/java/com/adrar/api/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.adrar.api.repository;
+
+import com.adrar.api.model.Book;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends CrudRepository<Book,Long> {
+}
