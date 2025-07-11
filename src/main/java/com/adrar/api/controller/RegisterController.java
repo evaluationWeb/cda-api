a9acd1878632cfa0ea677013eda623bd26499cbe
mithridatem <mathieumithridate@adrar-formation.com>
2025-07-11 15:02:10 +0200

    feat : endpoint Test (vérifier le token JWT)

--- src/main/java/com/adrar/api/controller/RegisterController.java
+++ src/main/java/com/adrar/api/controller/RegisterController.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -57,5 +58,10 @@ public class RegisterController {
                 .status(HttpStatus.CREATED)
                 .body(response);
     }
-
+    @GetMapping("/test")
+    public ResponseEntity<String> test(){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Test successful");
+    }
 }
